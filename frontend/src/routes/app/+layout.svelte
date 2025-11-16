9952f3667d9d8b9f79a26320c0588bdfbdbc2534
Patrick Diebold <5159715+PaHell@users.noreply.github.com>
2025-11-16 18:40:32 +0100

    fixed imports

--- frontend/src/routes/app/+layout.svelte
+++ frontend/src/routes/app/+layout.svelte
@@ -15,7 +15,7 @@
 	import { Calendar } from '$lib/shadcn/components/ui/calendar/index.js';
 	import * as Popover from '$lib/shadcn/components/ui/popover/index.js';
 	import type { FormEventHandler } from 'svelte/elements';
-	import { StockSymbolClient, type SymbolSearchMatch } from '@/clients';
+	import { StockClient, type SymbolLookup } from '@/clients';
 	import { debounce } from '@/custom/debounce';
 	import { env } from '$env/dynamic/public';
 	import * as Command from '$lib/shadcn/components/ui/command/index.js';
@@ -26,18 +26,22 @@
 	let searchValue = $state('');
 	let searchTrigger: HTMLDivElement | null = $state(null);
 	let searchContent: HTMLDivElement | null = $state(null);
-	let searchResults = $state<SymbolSearchMatch[]>([]);
+	let searchResults = $state<SymbolLookup | undefined>();
+	let isSearching = $state(false);
 
 	let debouncedOnSearch = debounce(onSearch, 100);
 
 	async function onSearch() {
 		try {
+			isSearching = true;
 			console.log('Searching for:', searchValue);
-			const client = new StockSymbolClient(env.PUBLIC_BACKEND_URL_CLIENT);
+			const client = new StockClient(env.PUBLIC_BACKEND_URL_CLIENT);
 			searchResults = await client.search(searchValue);
 			console.log('Search results:', searchResults);
 		} catch (error) {
 			console.error('Error searching symbols:', error);
+		} finally {
+			isSearching = false;
 		}
 	}
 </script>
@@ -68,9 +72,9 @@
 							<InputGroup.Addon>
 								<SearchIcon />
 							</InputGroup.Addon>
-							{#if searchResults.length > 0}
+							{#if searchResults?.count ?? 0 > 0}
 								<InputGroup.Addon align="inline-end"
-									>{searchResults.length} results</InputGroup.Addon
+									>{searchResults?.count ?? 0} results</InputGroup.Addon
 								>
 							{/if}
 						</InputGroup.Root>
@@ -78,15 +82,19 @@
 							<div
 								class="mt-px origin-(--bits-popover-content-transform-origin) rounded-md border bg-popover p-1 text-popover-foreground shadow-md outline-hidden data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95"
 							>
-								{#each searchResults as result}
+								{#each searchResults?.result ?? [] as result}
 									<a
 										href={`/app/stocks/${result.symbol}`}
 										class="relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-highlighted:bg-accent data-highlighted:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 data-[variant=destructive]:text-destructive data-[variant=destructive]:data-highlighted:bg-destructive/10 data-[variant=destructive]:data-highlighted:text-destructive dark:data-[variant=destructive]:data-highlighted:bg-destructive/20 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 [&_svg:not([class*='text-'])]:text-muted-foreground data-[variant=destructive]:*:[svg]:!text-destructive"
 									>
-										{result.name}
+										{result.description} ({result.displaySymbol})
 									</a>
 								{/each}
-								{#if searchResults.length === 0}
+								{#if isSearching}
+									<div class="px-2 py-1 text-sm text-muted-foreground">Searching...</div>
+								{:else if !searchValue.length}
+									<div class="px-2 py-1 text-sm text-muted-foreground">Type to search.</div>
+								{:else if searchResults?.count === 0}
 									<div class="px-2 py-1 text-sm text-muted-foreground">No results found.</div>
 								{/if}
 							</div>
